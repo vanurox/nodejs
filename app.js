ea390e0d0906e83a8501066d4e37275d9f2a3e24
cto_varun <cto.varun@gmail.com>
2019-02-20 16:34:33 +0530

    saved post in mongodb

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Post = require("./models/Post");
 const app = express();
+
+
+mongoose.connect("mongodb://localhost:27017/posts", { useNewUrlParser: true })
+    .then(() => {
+        console.log("Connection established successfully");
+    })
+    .catch(() => {
+        console.log("Error while establishing connection to mongodb");
+    })
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
     next();
 });
 
-app.get("/api/posts", (req, res, next) => {
-    // var post = req.body;
-    var posts = [
-        {
-            id: 1,
-            title: "First Post",
-            post: "Regarding March event"
-        },
-        {
-            id: 1,
-            title: "Social Work",
-            post: "Regarding Society"
-        }
-    ];
-    res.status(200).json({
-        msg: 1,
-        posts: posts
+app.post("/api/posts", (req, res, next) => {
+    const post = new Post({
+        title: req.body.title,
+        post: req.body.post
     });
+
+    post.save()
+        .then((result) => {
+            res.status(200).json({
+                msg: 1,
+                posts: post
+            })
+        })
+        .catch((err) => {
+            res.status(422).json({
+                error: err
+            });
+        });
+
 });
 
 module.exports = app;
---
+++ models/Post.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    post: { type: String, required: true }
+});
+
+
+module.exports = mongoose.model('Post', postSchema);
